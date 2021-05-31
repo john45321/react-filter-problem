a546924ec9e2df55f88976a405b0e9b9d6ff4425
john45321 <ha0983168@gmail.com>
2021-05-31 20:13:04 +0500

    Added values key for the checkboxes of different types

--- src/data.js
+++ src/data.js
@@ -35,32 +35,38 @@ export const listCheckboxesRating = [
     {
         id: 0,
         name: 'rating',
-        label: 'Any Rating'
+        label: 'Any Rating',
+        value: 0 // This checkbox value help us to match the movies rating field.
     },
     {
         id: 1,
         name: 'rating1',
-        label: 'Rating 1'
+        label: 'Rating 1',
+        value: 1
     },
     {
         id: 2,
         name: 'rating2',
-        label: 'Rating 2'
+        label: 'Rating 2',
+        value: 2
     },
     {
         id: 3,
         name: 'rating3',
-        label: 'Rating 3'
+        label: 'Rating 3',
+        value: 3
     },
     {
         id: 4,
         name: 'rating4',
-        label: 'Rating 4'
+        label: 'Rating 4',
+        value: 4
     },
     {
         id: 5,
         name: 'rating5',
-        label: 'Rating 5'
+        label: 'Rating 5',
+        value: 5
     }
 ];
 
@@ -68,26 +74,31 @@ export const listCheckboxesGenre = [
     {
         id: 0,
         name: 'genre',
-        label: 'Any Genre'
+        label: 'Any Genre',
+        value: '' // This checkbox value help us to match the movies genre field.
     },
     {
         id: 1,
         name: 'action',
-        label: 'Action'
+        label: 'Action',
+        value: 'Action'
     },
     {
         id: 2,
         name: 'comedy',
-        label: 'Comedy'
+        label: 'Comedy',
+        value: 'Comedy'
     },
     {
         id: 3,
         name: 'drama',
-        label: 'Drama'
+        label: 'Drama',
+        value: 'Drama'
     },
     {
         id: 4,
         name: 'thriller',
-        label: 'Thriller'
+        label: 'Thriller',
+        value: 'Thriller'
     }
 ];
